da5623ac42c5f0f6659e15d5665d614d8355697e
zach-s-app <zach18481@gmail.com>
2026-04-09 12:34:11 -0500

    Update JRuby version in run-polyglot.sh

--- run-polyglot.sh
+++ run-polyglot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # run-polyglot.sh — run the polyglot JS example in Rhino
 echo 'load(["libraries-Rhino.js"])//to import the java libraries'
-java --enable-native-access=ALL-UNNAMED -cp "rhino-all-1.9.1.jar:jython-standalone.jar:luaj-jse-3.0.2.jar:jruby-complete-10.0.4.0.jar" \
+java --enable-native-access=ALL-UNNAMED -cp "rhino-all-1.9.1.jar:jython-standalone.jar:luaj-jse-3.0.2.jar:jruby-complete-10.1.0.0-SNAPSHOT.jar" \
      org.mozilla.javascript.tools.shell.Main
